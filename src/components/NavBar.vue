e750d5e1a528111b95ffab8e9d415a38750ec03e
Luigi Battaglioli <lbattaglioli2000@gmail.com>
2020-02-12 11:05:25 -0500

    initial commit

---
+++ src/components/NavBar.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="bg-white text-black py-4 shadow">
+        The RED Bookshelf
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "NavBar"
+    }
+</script>
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        'tan': '#EEE2D2',
+        'red': '#D0021B',
+        'dark-red': '#8D0011',
+        'gray': '#4A4A4A'
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
